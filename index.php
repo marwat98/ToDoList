51c00600d7c75adfca61943724961cbef7be221c
marwat98 <marc.wat001@gmail.com>
2025-03-07 22:41:05 +0100

    add pages to route

--- index.php
+++ index.php
@@ -3,45 +3,37 @@
     use ShowNotes\ShowContent;
     use Steampixel\Route;
 
-
-    // try {
-        
-    //     $id = isset($_GET['modal_id']) ? $_GET['modal_id'] : null;
-
-    //     $sqlSelect = "SELECT * FROM addToDataBase";
-    //     $showContent = new ShowContent($twig);
-    //     $data = $showContent->dataBaseContent($sqlSelect, "message.html.twig");
-
-    //     $updateQuery = "SELECT * FROM addToDataBase WHERE id = '$id'";
-    //     $update = $showContent->dataBaseContent($updateQuery, "message.html.twig");
-
-    // } catch (Exception $e) {
-    //     $twig->display('message.html.twig',['message' => $e->getMessage()]);
-    //     exit();
-    // }
-
-    // $twig->display('header.html.twig');
-
-    // echo $twig->render('addNote.html.twig', ['data' => $data]);
-
-    
-    // echo $twig->render('modal.html.twig', ['data' => $id]);
-
-    // echo $twig->display('scripts.html.twig');
-    // echo $twig->display('footer.html.twig');
-
     Route::add('/',function(){
-        echo "Strona główna";
+        global $twig;
+        $twig->display('home.html.twig');
     });
     Route::add('/login',function(){
-        echo "Strona logowania";
+        global $twig;
+        $twig->display('login.html.twig');
     });
     Route::add('/register',function(){
-        echo "Strona rejestracji";
+        global $twig;
+        $twig->display('register.html.twig');
     });
-    Route::add('/addNote',function(){
-        echo "Dodaj liste zakupów";
+    Route::add('/add',function(){
+       global $twig;
+        try {
+            $id = isset($_GET['modal_id']) ? $_GET['modal_id'] : null;
+
+            $sqlSelect = "SELECT * FROM addToDataBase";
+            $showContent = new ShowContent($twig);
+            $data = $showContent->dataBaseContent($sqlSelect, "message.html.twig");
+
+            echo $twig->render('addNote.html.twig', ['data' => $data]);
+            echo $twig->render('modal.html.twig', ['data' => $id]);
+
+        } catch (Exception $e) {
+            $twig->display('message.html.twig',['message' => $e->getMessage()]);
+            exit();
+        }
+
     });
+    
     Route::run('/ToDoList');
 
 ?>
